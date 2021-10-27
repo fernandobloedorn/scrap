ea1427ae268ba91c190124f63a4b3e0240446f8c
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-27 20:16:17 -0300

    Add files via upload

--- test-crontab.py
+++ test-crontab.py
@@ -1,5 +1,5 @@
 from utils import save_file
 from time import gmtime, strftime
 
-save_file(strftime("%Y-%m-%d %H:%M:%S", gmtime()), "fer.txt")
+save_file(strftime("%Y-%m-%d %H:%M:%S", gmtime()), "/home/ubuntu/python/scrap/fer.txt")
 
