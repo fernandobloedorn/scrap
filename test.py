751face9907189798359448e0e23d919ed6f7c3a
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-27 13:14:58 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -18,6 +18,10 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("start-maximized"); 
+chrome_options.add_argument("disable-infobars");
+chrome_options.add_argument("--disable-extensions");
+chrome_options.add_argument("--disable-gpu"); 
  
 def buscaDados():
 
@@ -37,7 +41,7 @@ def buscaDados():
         print("ERROR:", e)
 
 try:
-    driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',chrome_options=chrome_options)
+    driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
     buscaDados()
 
 except Exception as e:
