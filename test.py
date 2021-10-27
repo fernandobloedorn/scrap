679634f3964d900848527c5db909feab5303f3d4
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-27 13:21:49 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("--remote-debugging-port=9222") 
 chrome_options.add_argument("start-maximized"); 
 chrome_options.add_argument("disable-infobars");
 chrome_options.add_argument("--disable-extensions");
