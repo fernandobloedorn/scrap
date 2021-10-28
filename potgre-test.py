7b188d19ccaba5a0349ad94d881f44d78e6fddba
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-28 15:48:16 -0300

    Add files via upload

--- potgre-test.py
+++ potgre-test.py
@@ -1,7 +1,15 @@
-hostname = 'localhost'
-username = 'python'
-password = 'c0d4@2021!'
-database = 'mais_trading'
+# hostname = 'localhost'
+# username = 'python'
+# password = 'c0d4@2021!'
+# database = 'mais_trading'
+
+# PG_HOST=localhost
+# PG_USER=python
+# PG_PASS=c0d4@2021!
+# PG_DB=mais_trading
+
+import os
+from dotenv import load_dotenv
 
 # Simple routine to run a query on a database and print the results:
 def doQuery( conn ) :
@@ -12,9 +20,10 @@ def doQuery( conn ) :
     for id, name in cur.fetchall() :
         print( id, name )
 
+load_dotenv()
 
 print( "Using psycopg:" )
 import psycopg
-myConnection = psycopg.connect( host=hostname, user=username, password=password, dbname=database )
+myConnection = psycopg.connect( host=os.environ['PG_HOST'], user=os.environ['PG_USER'], password=os.environ['PG_PASS'], dbname=os.environ['PG_DB'] )
 doQuery( myConnection )
 myConnection.close()
