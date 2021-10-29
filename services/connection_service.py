853f4f56375bf18cf130c6b3868ae27a23f57622
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-29 10:48:47 -0300

    Add files via upload

--- services/connection_service.py
+++ services/connection_service.py
@@ -1,9 +1,6 @@
 import psycopg
 import os
 
-def getConnectin():
-
-    print(os.environ['PG_HOST'], os.environ['PG_USER'], os.environ['PG_PASS'], os.environ['PG_DB'])
-
+def getConnection():
     connection = psycopg.connect( host=os.environ['PG_HOST'], user=os.environ['PG_USER'], password=os.environ['PG_PASS'], dbname=os.environ['PG_DB'] )
     return connection
