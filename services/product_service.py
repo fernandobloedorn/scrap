3881be9eafc12c64ee3ee524d75cf1617eb5b939
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-28 21:06:19 -0300

    Add files via upload

--- services/product_service.py
+++ services/product_service.py
@@ -2,11 +2,14 @@ from .connection_service import getConnectin
 
 def saveOrUpdate(produtos):
     conn = getConnectin()
+
     for p in produtos:
-        print(p.codigo, p.produto)
+        print(p.codigo, "-", p.produto)
         cur = conn.cursor()
-        cur.execute( "SELECT cadastra_produto('" + p.codigo + "', '" + p.produto + "', '', '" + p.ref + "', 0.0, '', '');" )
-        print("Salvou")
+        sql = "SELECT cadastra_produto('" + p.codigo + "', '" + p.produto + "', '', '" + p.ref + "', 0.0, '', '');"
+        print(sql)
+        cur.execute(sql)
+        print("Salvou 1")
     conn.close()
 
 # def createOrReplaceFunction():
