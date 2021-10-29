61de72bd6292791724286eb58bb47358caa88ed7
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-29 11:15:20 -0300

    Add existing file

--- pg-test-select.py
+++ pg-test-select.py
@@ -7,6 +7,7 @@ from models.product import Product
 load_dotenv()
 
 conn = getConnection()
+
 cur = conn.cursor()
 cur.execute("SELECT id FROM produto WHERE codigo = %s", ("12",))
 
@@ -14,12 +15,21 @@ result = cur.fetchone()
 
 if result is not None:
     id = result[0]
-    print(type(id))
+    print("ID:", id, "Type:", type(id))
+cur.close()
+
+print("-----------------")
+
+cur = conn.cursor()
+cur.execute("INSERT INTO produto (codigo, nome_tecnico) VALUES( %s, %s) RETURNING id;", ("13", "Teste 13"))
+id = cur.fetchone()[0]
+print("ID insert:", id, "Type:", type(id))
+cur.close()
 
 # for id in cur.fetchall() :
 #     print(type(id))
 #     print("ID:", id )
 
-cur.close()
+
 conn.close()
 
