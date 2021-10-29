0f57c040f924a224a9563f9c6615d449f4af140f
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-29 10:47:28 -0300

    Add files via upload

---
+++ pg-test-select.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from services.product_service import saveOrUpdate
+from services.connection_service import getConnection
+from models.product import Product
+
+load_dotenv()
+
+conn = getConnection()
+cur = conn.cursor()
+cur.execute("SELECT id FROM produto WHERE codigo = %s", "12")
+
+for id in cur.fetchall() :
+    print("ID:", id )
+
+conn.close()
+
