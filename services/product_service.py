3afc075ad260f675a61d5bee3c36d333824c4fe6
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-10-28 21:32:03 -0300

    Add files via upload

--- services/product_service.py
+++ services/product_service.py
@@ -2,25 +2,27 @@ from .connection_service import getConnectin
 
 def saveOrUpdate(produtos):
     
-    conn = getConnectin()
-    cur = conn.cursor()
-    sql = "SELECT cadastra_produto('12', 'teste', '', 'A', 0.0, '', '');"
-    # sql = "Insert into produto (codigo, nome_tecnico) values ('45', 'teste');"
-    print(sql)
-    cur.execute(sql)
-    conn.commit() # <- We MUST commit to reflect the inserted data
-    cur.close()
-    conn.close()
+    # conn = getConnectin()
+    # cur = conn.cursor()
+    # sql = "SELECT cadastra_produto('12', 'teste', '', 'A', 0.0, '', '');"
+    # # sql = "Insert into produto (codigo, nome_tecnico) values ('45', 'teste');"
+    # print(sql)
+    # cur.execute(sql)
+    # conn.commit() # <- We MUST commit to reflect the inserted data
+    # cur.close()
+    # conn.close()
 
-    # for p in produtos:
-    #     print(p.codigo, "-", p.produto)
-    #     conn = getConnectin()
-    #     cur = conn.cursor()
-    #     sql = "SELECT cadastra_produto('" + p.codigo + "', '" + p.produto + "', '', '" + p.ref + "', 0.0, '', '');"
-    #     print(sql)
-    #     cur.execute(sql)
-    #     conn.close()
-    #     print("Salvou 1")
+    for p in produtos:
+        print(p.codigo, "-", p.produto)
+        conn = getConnectin()
+        cur = conn.cursor()
+        sql = "SELECT cadastra_produto('" + p.codigo + "', '" + p.produto + "', '', '" + p.ref + "', 0.0, '', '');"
+        print(sql)
+        cur.execute(sql)
+        conn.commit()
+        cur.close()
+        conn.close()
+        print("Salvou 1")
         
 
 # def createOrReplaceFunction():
