ce2c44a2c8140181df0a49dc43875de2b31e07c9
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-11-02 11:13:26 -0300

    Add existing file

--- services/product_service.py
+++ services/product_service.py
@@ -6,7 +6,7 @@ INSERT_PRODUCT = "INSERT INTO produto (codigo, nome_tecnico, referencia, linha, 
 UPDATE_PRODUCT = "UPDATE produto SET nome_tecnico = %s, referencia = %s, linha = %s, dt_alteracao = CURRENT_TIMESTAMP WHERE id = %s;"
 
 SELECT_LOT = "SELECT id FROM produto_lote WHERE produto_id = %s AND lote = %s;"
-INSERT_LOT = "INSERT INTO produto_lote (produto_id, lote, saldo_cdi, saldo_embramaco, dt_programacao, dt_cadastro, dt_alteracao) VALUES( %s, %s, %s, %s, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING id;"
+INSERT_LOT = "INSERT INTO produto_lote (produto_id, lote, saldo_cdi, saldo_embramaco, dt_programacao, dt_cadastro, dt_alteracao) VALUES( %s, %s, %s, %s, %s, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING id;"
 UPDATE_LOT = "UPDATE produto_lote SET saldo_cdi = %s, saldo_embramaco = %s, dt_programacao = %s, dt_alteracao = CURRENT_TIMESTAMP WHERE id = %s;"
 
 SELECT_LOT_INVENTORY = "SELECT id FROM produto_lote_saldo WHERE produto_lote_id = %s AND dt_estoque = %s;"
