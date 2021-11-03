6f8fd685afc2ea3b1448c28a1b0e643ecccec518
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-11-03 12:37:49 -0300

    Add existing file

---
+++ test-upload-file.py
@@ -0,0 +1,5 @@
+from api.google_drive.upload_file import upload_file
+
+# if products.__len__() > 0:
+#     fileUtil.productsToCsv("estoque_embramaco.csv", products)
+upload_file("estoque_embramaco.csv")
