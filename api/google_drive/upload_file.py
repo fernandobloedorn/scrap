7b5eaea6a804051376ec726c6f3a1110c3a1aa1e
Fernando Bloedorn <fernandobloedorn@gmail.com>
2021-11-04 12:39:12 -0300

    Add Services and Util

--- api/google_drive/upload_file.py
+++ api/google_drive/upload_file.py
@@ -13,6 +13,7 @@ def fileUpload(fileName, fileContent):
     # fileName    = 'com_content.txt'
     # fileContent = 'hello.txt'
 
-    file = drive.CreateFile({"parents":  [{'id': '1T9xL8H5cSmqJAyVRCun8MTqBgzYiv9-w'}], 'title': fileName}) # id da pasta Mais Trading
+    # file = drive.CreateFile({"parents":  [{'id': '1T9xL8H5cSmqJAyVRCun8MTqBgzYiv9-w'}], 'title': fileName}) # id da pasta Mais Trading
+    file = drive.CreateFile({"parents":  [{'id': '1T9xL8H5cSmqJAyVRCun8MTqBgzYiv9-w'}], 'title': fileName, 'id': '1O89Z8QK_qbOVqXNnrhW-6So_PZAazqfB'})
     file.SetContentFile(fileContent)
     file.Upload()
